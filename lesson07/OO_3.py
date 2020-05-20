cf4bfb155b2be0f51fc1386bd2106ca05bf9f5b3
LE-S34 <r208790@yahoo.com.tw>
2020-05-20 19:19:49 +0800

    python

--- lesson07/OO_3.py
+++ lesson07/OO_3.py
@@ -3,9 +3,10 @@ class Account:
     __balance = 100
 
     def __str__(self):
-        return self.name + '有 $' + str(self.__balance)
+        return self.name + ' 有 $' + str(self.__balance)
 
 if __name__ == '__main__' :
+    Account.bank = '元大銀行'
     acc = Account()
     acc.name = 'Edward'
-    print(acc)
+    print(Account.bank, acc)
