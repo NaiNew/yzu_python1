048b46d2e04892efd1961098de6f949cd7e0b46e
LE-S34 <r208790@yahoo.com.tw>
2020-05-20 19:04:25 +0800

    python

---
+++ lesson07/OO_1.py
@@ -0,0 +1,11 @@
+class Human:
+    name = ''
+    age = 0
+    sex = ''
+
+if __name__ == '__main__':
+    h = Human()
+    h.name = 'Edward'
+    h.sex = '男'
+    h.age = 27
+    print(h.name, h.sex, h.age)
---
+++ lesson07/OO_2.py
@@ -0,0 +1,7 @@
+import lesson07.OO_1 as o
+
+h = o.Human()
+h.name = 'Edward'
+h. sex = '男'
+h. age = 27
+print(h.name, h.sex, h.age)
