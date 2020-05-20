3f744e9d43b909341b3f5b0324a67a14263ad86c
LE-S34 <r208790@yahoo.com.tw>
2020-05-20 19:16:17 +0800

    python

--- lesson07/OO_1.py
+++ lesson07/OO_1.py
@@ -3,9 +3,13 @@ class Human:
     age = 0
     sex = ''
 
+    def __str__(self):
+        return self.name + ' ' + self.sex + ' ' + str(self.age)
+
 if __name__ == '__main__':
     h = Human()
     h.name = 'Edward'
     h.sex = '男'
     h.age = 27
     print(h.name, h.sex, h.age)
+    print(h)
